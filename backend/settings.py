e93b262a4982c7584ca335ad5a042b2e07146e0c
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-02 16:02:32 -0700

    Updated Auth user in settings

--- backend/settings.py
+++ backend/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'accounts.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
