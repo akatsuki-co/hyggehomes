9487bc71cf00a4b8a6688b302fe8b979de2ad558
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-03 03:05:03 -0700

    Connected css styling with index

--- backend/settings.py
+++ backend/settings.py
@@ -119,5 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates'),
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
--- backend/views.py
+++ backend/views.py
@@ -3,8 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    context = {
-        "title": "Hello World",
-        "content": "Welcome to the Home Page! Why Perfect?",
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
