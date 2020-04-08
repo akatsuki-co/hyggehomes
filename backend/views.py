6730e1806f8721c2f6cfaccf43ebf2b30669c0dc
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-07 22:23:06 -0700

    Connect stay instances to frontend

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.views.generic import ListView
+
+from apps.stays.models import Stay
 
 
 def explore_page(request):
@@ -8,3 +10,19 @@ def explore_page(request):
 
 def landing_page(request):
     return render(request, 'landing.html')
+
+
+class StayCityListView(ListView):
+    """Stay for each City List View"""
+    queryset = Stay.objects.all()
+    context_object_name = 'stays_list'
+    template_name = 'city.html'
+
+    def get_queryset(self):
+        qs = Stay.objects.all()
+        return qs.filter(city=self.kwargs['city'])
+
+    def get_context_data(self, *args, **kwargs):
+        """Method for getting context data"""
+        context = super().get_context_data(**kwargs)
+        return context
