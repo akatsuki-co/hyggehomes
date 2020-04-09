16d122e4a691f6aa62d799e4158965b0b0b25a87
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-09 10:15:47 -0700

    Add context data to city view

--- backend/views.py
+++ backend/views.py
@@ -14,14 +14,14 @@ def landing_page(request):
 
 class StayCityListView(ListView):
     """Stay for each City List View"""
+    queryset = Stay.objects.all()
     context_object_name = 'stays_list'
     template_name = 'city.html'
-
-    def get_queryset(self):
-        qs = Stay.objects.all()
-        return qs.filter(city=self.kwargs['city'])
 
     def get_context_data(self, *args, **kwargs):
         """Method for getting context data"""
         context = super().get_context_data(**kwargs)
+        city = self.kwargs['city']
+        context['stays_list'] = Stay.objects.all().filter(city=city)
+        context['city'] = city
         return context
