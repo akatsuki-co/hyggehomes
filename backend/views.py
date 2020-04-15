654e838ac33f208d5096c7c858d04343e2dabccf
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-15 15:23:57 -0700

    Move explore_cities dict to class attribute

--- backend/views.py
+++ backend/views.py
@@ -8,17 +8,18 @@ class ExploreView(ListView):
     """Several Lists of Stay Views"""
     queryset = Stay.objects.all()
     template_name = 'explore.html'
+    cities = [
+        {'Paris': 'https://unsplash.com/photos/milUxSbp4_A'},
+        {'New York': 'https://unsplash.com/photos/5r5554u-mHo'},
+        {'Sydney': 'https://unsplash.com/photos/qaNcz43MeY8'},
+        {'Cape Town': 'https://unsplash.com/photos/hzR9rDXWbqo'},
+        {'Tokyo': 'https://unsplash.com/photos/alY6_OpdwRQ'},
+    ]
 
     def get_context_data(self, *args, **kwargs):
         """Method for getting context data"""
         context = super().get_context_data(**kwargs)
-        explore_cities = [
-            {'Paris': 'https://unsplash.com/photos/milUxSbp4_A'},
-            {'New York': 'https://unsplash.com/photos/5r5554u-mHo'},
-            {'Sydney': 'https://unsplash.com/photos/qaNcz43MeY8'},
-            {'Cape Town': 'https://unsplash.com/photos/hzR9rDXWbqo'},
-            {'Tokyo': 'https://unsplash.com/photos/alY6_OpdwRQ'},
-        ]
+        explore_cities = self.explore_cities
         qs = Stay.objects.all()
         context['explore_cities'] = explore_cities
         context['sf_stays_list'] = qs.filter(city="San Francisco")[:4]
