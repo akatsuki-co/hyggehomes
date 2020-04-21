c7ecc92eec2d964bdbc58c4fcbd0332234aadaa1
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-20 20:36:22 -0700

    Move template file

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from apps.stays.models import Stay
+
+
+class TripsView(ListView):
+    """View for Showing all User trips"""
+    queryset = Stay.objects.all()
+    template_name = 'my_trips.html'
+
+    def get_context_data(self, *args, **kwargs):
+        """Method for getting context data"""
+        context = super().get_context_data(**kwargs)
+        qs = Stay.objects.all().prefetch_related('reviews')
+        context['sf_stays_list'] = qs.filter(city="San Francisco")[:4]
+        context['featured_stays_list'] = qs.filter(featured=True)[:4]
+        return context
+
