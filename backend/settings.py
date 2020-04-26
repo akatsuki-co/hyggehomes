b01911450823100c4aca85e95d9f6ac1e6a17a7e
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-25 22:45:24 -0700

    Download local database and upload to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xdxyhwco',
+        'USER': 'xdxyhwco',
+        'PASSWORD': 'eXQBFWLGxYwoMub-aOH8-c--3teK8sRh',
+        'HOST': 'drona.db.elephantsql.com',
+        'PORT': '5432'
+
     }
 }
 
